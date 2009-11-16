d44b4fa1ce20fa880eb928a616df906dabbbbbfe
Timmcd <tmcdowell@gmail.com>
2009-11-15 19:27:42 -0700

    Some problems with line cut-off.

--- goclient.go
+++ goclient.go
@@ -9,16 +9,21 @@ import (
 	)
 
 func main() {
+	fmt.Print("\x1B[0;58r");
+	fmt.Print("\x1B[59;1H");
+	fmt.Print("---------------------------------------------------------------------------------");
 	raddr, _ := net.ResolveTCPAddr(flag.Arg(0));
 	conn, _ := net.DialTCP("tcp", nil, raddr);
 	bytec := make(chan int);
 	go readLoop(bytec, conn);
 	go writeLoop(conn);
 	<-bytec;
+	fmt.Print("\x1B[r");
 }
 
 func readLoop(bytec chan int, conn *net.TCPConn) {
 	buf := make([]byte, 1);
+	var out string;
 	for {
 		_, err := conn.Read(buf);
 		if err != nil {
@@ -27,7 +32,13 @@ func readLoop(bytec chan int, conn *net.TCPConn) {
 				break;
 			}
 		}
-		fmt.Print(string(buf[0]));
+		out += string(buf[0]);
+		if string(buf[0]) == "\n" {
+			fmt.Print("\x1B[58;1H");
+			fmt.Print(out);
+			fmt.Print("\x1B[60;1H");
+			out = "";
+		}
 	}
 	bytec <- 1;
 }
